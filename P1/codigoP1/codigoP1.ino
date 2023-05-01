78ff5378b025b1dd5871ab6bde85cb44745ec23e
luis-carbajal-c <luis.carbajal@utec.edu.pe>
2023-04-30 21:39:26 -0500

    P1 actualizado

--- P1/codigoP1/codigoP1.ino
+++ P1/codigoP1/codigoP1.ino
@@ -4,11 +4,11 @@ const int vehiculoAmarillo = 9;
 const int vehiculoVerde = 10;
 const int personaRojo = 12;
 const int personaVerde = 13;
+const int flag = 2;
 
-// definicion de tiempos de espera, semaforo de vehiculos como referencia
-const int tiempoRojo = 55.4*1000;
-const int tiempoAmarillo = 3*1000;
-const int tiempoVerde = 45.78*1000;
+float tiempoRojo = 0;
+float tiempoAmarillo = 0;
+float tiempoVerde = 0;
 
 void setup() {
   pinMode(vehiculoRojo, OUTPUT);
@@ -16,9 +16,22 @@ void setup() {
   pinMode(vehiculoVerde, OUTPUT);
   pinMode(personaRojo, OUTPUT);
   pinMode(personaVerde, OUTPUT);
+  pinMode(flag, INPUT);
 }
 
 void loop() {
+  int dr = digitalRead(flag);
+  Serial.println(dr);
+  if (dr == 1) {
+	tiempoRojo = 500;
+	tiempoAmarillo = 0;
+	tiempoVerde = 500;
+  } else {
+    	tiempoRojo = 55.4*1000;
+	tiempoAmarillo = 3*1000;
+	tiempoVerde = 45.78*1000;
+  }
+
   // luz de vehiculo en rojo, luz de persona en verde
   digitalWrite(vehiculoAmarillo, LOW); 
   digitalWrite(vehiculoRojo, HIGH);
