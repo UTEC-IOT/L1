d42969d33e1b1117374c3118f1db09e7ea5eb75e
luis-carbajal-c <luis.carbajal@utec.edu.pe>
2023-04-30 20:16:03 -0500

    solucion P2

--- P2/codigoP2/codigoP2.ino
+++ P2/codigoP2/codigoP2.ino
@@ -1,38 +1,25 @@
-// definicion de pines para los semaforos
-const int vehiculoRojo = 8;
-const int vehiculoAmarillo = 9;
-const int vehiculoVerde = 10;
-const int personaRojo = 12;
-const int personaVerde = 13;
-
-// definicion de tiempos de espera, semaforo de vehiculos como referencia
-const int tiempoRojo = 55.4*1000;
-const int tiempoAmarillo = 3*1000;
-const int tiempoVerde = 45.78*1000;
+const int pinAnalog = A0;
 
 void setup() {
-  pinMode(vehiculoRojo, OUTPUT);
-  pinMode(vehiculoAmarillo, OUTPUT);
-  pinMode(vehiculoVerde, OUTPUT);
-  pinMode(personaRojo, OUTPUT);
-  pinMode(personaVerde, OUTPUT);
+  Serial.begin(9600);
+
+  pinMode(pinAnalog, INPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
 }
 
 void loop() {
-  // luz de vehiculo en rojo, luz de persona en verde
-  digitalWrite(vehiculoAmarillo, LOW); 
-  digitalWrite(vehiculoRojo, HIGH);
-  digitalWrite(personaRojo, LOW);
-  digitalWrite(personaVerde, HIGH); 
-  delay(tiempoRojo);
-  // luz de vehiculo en verde, luz de persona en rojo
-  digitalWrite(vehiculoVerde, HIGH); 
-  digitalWrite(vehiculoRojo, LOW);
-  digitalWrite(personaRojo, HIGH);
-  digitalWrite(personaVerde, LOW); 
-  delay(tiempoVerde);
-  // luz de vehiculo en amarillo, luz de persona en rojo
-  digitalWrite(vehiculoVerde, LOW); 
-  digitalWrite(vehiculoAmarillo, HIGH);
-  delay(tiempoAmarillo);
+  int lectura = analogRead(pinAnalog);
+  int valorDigital = map(lectura, 0, 1023, 0, 15);
+  
+  // vincular el pin con el bit
+  digitalWrite(2, bitRead(valorDigital, 0));
+  digitalWrite(3, bitRead(valorDigital, 1));
+  digitalWrite(4, bitRead(valorDigital, 2));
+  digitalWrite(5, bitRead(valorDigital, 3));
+  
+  Serial.println(valorDigital);
+  delay(100);
 }
